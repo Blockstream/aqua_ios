af930306546cf093e7d81760af9b0d670b3ee689
Luca Vaccaro <me@lvaccaro.com>
2020-06-12 18:57:54 +0200

    Add settings page

--- aquaios/Settings/SettingsViewController.swift
+++ aquaios/Settings/SettingsViewController.swift
@@ -2,20 +2,96 @@ import UIKit
 
 class SettingsViewController: BaseViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var removeLabel: UILabel!
+
+    var labels = ["Contact support",
+                  "View my recovery phrase",
+                  "Device authorization"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
 
-        // Do any additional setup after loading the view.
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.removeWallet))
+        removeLabel.addGestureRecognizer(tapGestureRecognizer)
+        removeLabel.isUserInteractionEnabled = true
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.title = NSLocalizedString("Settings", comment: "")
+        navigationController?.setNavigationBarHidden(false, animated: false)
     }
-    */
 
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.tableFooterView = UIView()
+    }
+
+    @objc func removeWallet(_ sender: Any?) {
+        let alert = UIAlertController(title: "Warning", message: "Are you sure to remove your wallet? The operation is not reversible and the app will be closed.", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("id_cancel", comment: ""), style: .cancel) { _ in })
+        alert.addAction(UIAlertAction(title: NSLocalizedString("id_continue", comment: ""), style: .default) { _ in
+            exit(0)
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func enableSafeMnemonic() {
+        let alert = UIAlertController(title: "Warning", message: "Enable access protection?", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("id_cancel", comment: ""), style: .cancel) { _ in })
+        alert.addAction(UIAlertAction(title: NSLocalizedString("id_continue", comment: ""), style: .default) { _ in
+
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func disableSafeMnemonic() {
+        let alert = UIAlertController(title: "Warning", message: "Disable access protection?", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("id_cancel", comment: ""), style: .cancel) { _ in })
+        alert.addAction(UIAlertAction(title: NSLocalizedString("id_continue", comment: ""), style: .default) { _ in
+
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return labels.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = labels[indexPath.row]
+        if indexPath.row == 2 {
+            let switcher = UISwitch()
+            switcher.isOn = true
+            cell?.accessoryView = switcher
+        }
+        return cell ?? UITableViewCell()
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            // open email to support
+            let email = "support@greenaddress.it"
+            if let url = URL(string: "mailto:\(email)") {
+              UIApplication.shared.open(url)
+            }
+        case 1:
+            // view mnemonic
+            performSegue(withIdentifier: "mnemonic", sender: nil)
+        case 2:
+            // device authorization
+            break
+        default:
+            return
+        }
+    }
 }
