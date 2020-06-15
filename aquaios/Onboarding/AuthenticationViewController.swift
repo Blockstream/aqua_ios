95f3b38ea62a92768b714ff00d3224baedd7fecb
Domenico Gabriele <domenico@blockstream.com>
2020-06-15 16:59:43 +0200

    Layout authentication view

--- aquaios/Onboarding/AuthenticationViewController.swift
+++ aquaios/Onboarding/AuthenticationViewController.swift
@@ -1,6 +1,13 @@
 import Foundation
+import UIKit
+import LocalAuthentication
 
 class AuthenticationViewController: BaseViewController {
+
+    @IBOutlet weak var enableButton: UIButton!
+    @IBOutlet weak var authIcon: UIImageView!
+    @IBOutlet weak var authLabel: UILabel!
+    @IBOutlet weak var authDescriptionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -8,6 +15,21 @@ class AuthenticationViewController: BaseViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        enableButton.round(radius: 26.5)
+        switch authType() {
+        case .touchID:
+            authLabel.text = "Enable Touch ID"
+            authDescriptionLabel.text = "Secure your aqua wallet with a simple thumb press"
+            authIcon.image = UIImage(named: "touchid")
+        case .faceID:
+            authLabel.text = "Enable Face ID"
+            authDescriptionLabel.text = "The fastest wasy to secure your AQUA wallet"
+            authIcon.image = UIImage(named: "faceid")
+        default:
+            authLabel.text = "Enable Passcode"
+            authDescriptionLabel.text = "A quick and easy way to secure your AQUA wallet"
+            authIcon.image = UIImage(named: "passcode")
+        }
     }
 
     func storeMnemonic(safe: Bool) {
@@ -28,6 +50,16 @@ class AuthenticationViewController: BaseViewController {
                 showError(error.localizedDescription)
             }
         }
+    }
+
+    func authType() -> LABiometryType {
+        let context = LAContext()
+
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return LABiometryType.none
+        }
+        return context.biometryType
     }
 
     @IBAction func enable(_ sender: Any) {
