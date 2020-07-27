ea08f924760b52c9822463dfbc49a211e070521b
Domenico Gabriele <domenico@blockstream.com>
2020-07-27 17:31:40 +0200

    Add pre login component to buy tab

--- aquaios/Buy/BuyViewController.swift
+++ aquaios/Buy/BuyViewController.swift
@@ -7,7 +7,8 @@ class BuyViewController: BaseViewController {
     @IBOutlet weak var actionStackBackgroundView: UIView!
     @IBOutlet weak var btcButton: UIButton!
     @IBOutlet weak var lbtcButton: UIButton!
-
+    @IBOutlet weak var buyView: UIView!
+    @IBOutlet weak var buyIconView: UIView!
     private var wyreService: WyreService!
 
     override func viewDidLoad() {
@@ -16,9 +17,21 @@ class BuyViewController: BaseViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         actionStackBackgroundView.round(radius: 21, borderWidth: 2, borderColor: .tiffanyBlue)
-        infoLabel.text = "Checking availability..."
-        wyreService.getWidget()
+        configurePreLogin()
+        if hasWallet {
+            buyView.isHidden = true
+            btcButton.isHidden = false
+            lbtcButton.isHidden = false
+            infoLabel.isHidden = false
+            infoLabel.text = "Checking availability..."
+            wyreService.getWidget()
+        } else {
+            btcButton.isHidden = true
+            lbtcButton.isHidden = true
+            infoLabel.isHidden = true
+        }
     }
 
     @IBAction func actionButtonTapped(_ sender: UIButton) {
@@ -30,6 +43,18 @@ class BuyViewController: BaseViewController {
         if let dest = segue.destination as? WyreWidgetViewController {
             dest.buyBtc = sender as? Bool
         }
+    }
+
+    func configurePreLogin() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.createOrRestore))
+        buyView.addGestureRecognizer(tapGestureRecognizer)
+        buyView.isUserInteractionEnabled = true
+        buyView.round(radius: 24)
+        buyIconView.round(radius: 24)
+    }
+
+    @objc func createOrRestore(_ sender: Any?) {
+        performSegue(withIdentifier: "create_wallet_alert", sender: nil)
     }
 }
 
